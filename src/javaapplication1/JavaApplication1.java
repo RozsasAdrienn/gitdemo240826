83c3c29ac4627e388bd4be7d4ee61772af98f162
RozsasAdrienn <rozsa.paradoxon@gmail.com>
2024-08-26 15:05:14 +0200

    projekt

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,17 @@
+
+package javaapplication1;
+
+
+public class JavaApplication1 {
+
+    
+    public static void main(String[] args) {
+
+        new JavaApplication1().kiir();
+    }
+    
+     public void kiir() {
+        System.out.println("");
+    }
+    
+}
